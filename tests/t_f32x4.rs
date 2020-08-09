76482f391aa3004b2258dce325d6634a94589395
Lokathor <zefria@gmail.com>
2020-08-09 00:40:05 -0600

    update CI (this should work as a config but fail to build on i586)

--- tests/t_f32x4.rs
+++ tests/t_f32x4.rs
@@ -1,6 +1,12 @@
 use wide::*;
 
 #[test]
+fn size_align() {
+  assert_eq!(core::mem::size_of::<f32x4>(), 16);
+  assert_eq!(core::mem::align_of::<f32x4>(), 16);
+}
+
+#[test]
 fn impl_add_for_f32x4() {
   let a = f32x4::from([1.0, 2.0, 3.0, 4.0]);
   let b = f32x4::from([5.0, 6.0, 7.0, 8.0]);
