7d586f967d0fa5a69a93c904a052d05907891a35
Lokathor <zefria@gmail.com>
2022-10-07 00:13:08 -0600

    join all tests into a single test binary. this makes them run *much* faster

---
+++ tests/all_tests/main.rs
@@ -0,0 +1,23 @@
+#![allow(clippy::approx_constant)]
+#![allow(clippy::unnecessary_cast)]
+#![allow(clippy::assertions_on_constants)]
+
+mod t_f32x4;
+mod t_f32x8;
+mod t_f64x2;
+mod t_f64x4;
+mod t_i16x16;
+mod t_i16x8;
+mod t_i32x4;
+mod t_i32x8;
+mod t_i64x2;
+mod t_i64x4;
+mod t_i8x16;
+mod t_i8x32;
+mod t_u16x8;
+mod t_u32x4;
+mod t_u32x8;
+mod t_u64x2;
+mod t_u64x4;
+mod t_u8x16;
+mod t_usefulness;
