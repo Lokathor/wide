305965c2538316c70d72dc284b73ceb1129fdc2f
Kristof <kristofr@gmail.com>
2024-05-13 11:18:12 +0200

    fix test

--- tests/all_tests/t_i16x8.rs
+++ tests/all_tests/t_i16x8.rs
@@ -364,7 +364,7 @@ fn impl_mul_keep_high() {
 
 #[test]
 fn impl_i16x8_mul_widen() {
-  let a = i16x8::from([1, 2, 3, 4, 5, 6, i16::MIN, i16::MIN]);
+  let a = i16x8::from([1, 2, 3, 4, 5, 6, i16::MIN, i16::MAX]);
   let b = i16x8::from([17, -18, 190, -20, 21, -22, i16::MAX, i16::MAX]);
   let expected = i32x8::from([
     17,
@@ -374,7 +374,7 @@ fn impl_i16x8_mul_widen() {
     105,
     -132,
     (i16::MIN as i32) * (i16::MAX as i32),
-    (i16::MIN as i32) * (i16::MAX as i32),
+    (i16::MAX as i32) * (i16::MAX as i32),
   ]);
   let actual = a.mul_widen(b);
   assert_eq!(expected, actual);
