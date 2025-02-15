e6ecf18c5b5bfcd34f448a644d987ad6aba1bac8
Genna Wingert <1058083+wingertge@users.noreply.github.com>
2025-02-15 20:02:51 +0100

    Add u8x32 (#190)

---
+++ src/u8x32_.rs
@@ -0,0 +1,256 @@
+use super::*;
+
+pick! {
+  if #[cfg(target_feature="avx2")] {
+    #[derive(Default, Clone, Copy, PartialEq, Eq)]
+    #[repr(C, align(32))]
+    pub struct u8x32 { avx: m256i }
+  } else {
+    #[derive(Default, Clone, Copy, PartialEq, Eq)]
+    #[repr(C, align(32))]
+    pub struct u8x32 { a : u8x16, b : u8x16 }
+  }
+}
+
+int_uint_consts!(u8, 32, u8x32, 256);
+
+unsafe impl Zeroable for u8x32 {}
+unsafe impl Pod for u8x32 {}
+
+impl Add for u8x32 {
+  type Output = Self;
+  #[inline]
+  #[must_use]
+  fn add(self, rhs: Self) -> Self::Output {
+    pick! {
+      if #[cfg(target_feature="avx2")] {
+        Self { avx: add_i8_m256i(self.avx,rhs.avx) }
+      } else {
+        Self {
+          a : self.a.add(rhs.a),
+          b : self.b.add(rhs.b),
+        }
+      }
+    }
+  }
+}
+
+impl Sub for u8x32 {
+  type Output = Self;
+  #[inline]
+  #[must_use]
+  fn sub(self, rhs: Self) -> Self::Output {
+    pick! {
+      if #[cfg(target_feature="avx2")] {
+        Self { avx: sub_i8_m256i(self.avx,rhs.avx) }
+      } else {
+        Self {
+          a : self.a.sub(rhs.a),
+          b : self.b.sub(rhs.b),
+        }
+      }
+    }
+  }
+}
+
+impl Add<u8> for u8x32 {
+  type Output = Self;
+  #[inline]
+  #[must_use]
+  fn add(self, rhs: u8) -> Self::Output {
+    self.add(Self::splat(rhs))
+  }
+}
+
+impl Sub<u8> for u8x32 {
+  type Output = Self;
+  #[inline]
+  #[must_use]
+  fn sub(self, rhs: u8) -> Self::Output {
+    self.sub(Self::splat(rhs))
+  }
+}
+
+impl Add<u8x32> for u8 {
+  type Output = u8x32;
+  #[inline]
+  #[must_use]
+  fn add(self, rhs: u8x32) -> Self::Output {
+    u8x32::splat(self).add(rhs)
+  }
+}
+
+impl Sub<u8x32> for u8 {
+  type Output = u8x32;
+  #[inline]
+  #[must_use]
+  fn sub(self, rhs: u8x32) -> Self::Output {
+    u8x32::splat(self).sub(rhs)
+  }
+}
+
+impl BitAnd for u8x32 {
+  type Output = Self;
+  #[inline]
+  #[must_use]
+  fn bitand(self, rhs: Self) -> Self::Output {
+    pick! {
+      if #[cfg(target_feature="avx2")] {
+          Self { avx : bitand_m256i(self.avx,rhs.avx) }
+      } else {
+          Self {
+            a : self.a.bitand(rhs.a),
+            b : self.b.bitand(rhs.b),
+          }
+      }
+    }
+  }
+}
+
+impl BitOr for u8x32 {
+  type Output = Self;
+  #[inline]
+  #[must_use]
+  fn bitor(self, rhs: Self) -> Self::Output {
+    pick! {
+      if #[cfg(target_feature="avx2")] {
+        Self { avx : bitor_m256i(self.avx,rhs.avx) }
+      } else {
+        Self {
+          a : self.a.bitor(rhs.a),
+          b : self.b.bitor(rhs.b),
+        }
+      }
+    }
+  }
+}
+
+impl BitXor for u8x32 {
+  type Output = Self;
+  #[inline]
+  #[must_use]
+  fn bitxor(self, rhs: Self) -> Self::Output {
+    pick! {
+      if #[cfg(target_feature="avx2")] {
+        Self { avx : bitxor_m256i(self.avx,rhs.avx) }
+      } else {
+        Self {
+          a : self.a.bitxor(rhs.a),
+          b : self.b.bitxor(rhs.b),
+        }
+      }
+    }
+  }
+}
+
+impl CmpEq for u8x32 {
+  type Output = Self;
+  #[inline]
+  #[must_use]
+  fn cmp_eq(self, rhs: Self) -> Self::Output {
+    pick! {
+      if #[cfg(target_feature="avx2")] {
+        Self { avx : cmp_eq_mask_i8_m256i(self.avx,rhs.avx) }
+      } else {
+        Self {
+          a : self.a.cmp_eq(rhs.a),
+          b : self.b.cmp_eq(rhs.b),
+        }
+      }
+    }
+  }
+}
+
+impl u8x32 {
+  #[inline]
+  #[must_use]
+  pub const fn new(array: [u8; 32]) -> Self {
+    unsafe { core::intrinsics::transmute(array) }
+  }
+  #[inline]
+  #[must_use]
+  pub fn blend(self, t: Self, f: Self) -> Self {
+    pick! {
+      if #[cfg(target_feature="avx2")] {
+        Self { avx: blend_varying_i8_m256i(f.avx, t.avx, self.avx) }
+      } else {
+        Self {
+          a : self.a.blend(t.a, f.a),
+          b : self.b.blend(t.b, f.b),
+        }
+      }
+    }
+  }
+  #[inline]
+  #[must_use]
+  pub fn max(self, rhs: Self) -> Self {
+    pick! {
+      if #[cfg(target_feature="avx2")] {
+        Self { avx: max_u8_m256i(self.avx,rhs.avx) }
+      } else {
+        Self {
+          a : self.a.max(rhs.a),
+          b : self.b.max(rhs.b),
+        }
+      }
+    }
+  }
+  #[inline]
+  #[must_use]
+  pub fn min(self, rhs: Self) -> Self {
+    pick! {
+      if #[cfg(target_feature="avx2")] {
+        Self { avx: min_u8_m256i(self.avx,rhs.avx) }
+      } else {
+        Self {
+          a : self.a.min(rhs.a),
+          b : self.b.min(rhs.b),
+        }
+      }
+    }
+  }
+
+  #[inline]
+  #[must_use]
+  pub fn saturating_add(self, rhs: Self) -> Self {
+    pick! {
+      if #[cfg(target_feature="avx2")] {
+        Self { avx: add_saturating_u8_m256i(self.avx, rhs.avx) }
+      } else {
+        Self {
+          a : self.a.saturating_add(rhs.a),
+          b : self.b.saturating_add(rhs.b),
+        }
+      }
+    }
+  }
+  #[inline]
+  #[must_use]
+  pub fn saturating_sub(self, rhs: Self) -> Self {
+    pick! {
+      if #[cfg(target_feature="avx2")] {
+        Self { avx: sub_saturating_u8_m256i(self.avx, rhs.avx) }
+      } else {
+        Self {
+          a : self.a.saturating_sub(rhs.a),
+          b : self.b.saturating_sub(rhs.b),
+        }
+      }
+    }
+  }
+
+  #[inline]
+  pub fn to_array(self) -> [u8; 32] {
+    cast(self)
+  }
+
+  #[inline]
+  pub fn as_array_ref(&self) -> &[u8; 32] {
+    cast_ref(self)
+  }
+
+  #[inline]
+  pub fn as_array_mut(&mut self) -> &mut [u8; 32] {
+    cast_mut(self)
+  }
+}
