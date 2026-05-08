94c50a931e5cb5a58c1f7915582f1ff134beda03
Noam2Stein <noamsteindev@proton.me>
2026-05-08 19:15:27 +0300

    Add missing `Sum` trait implementation for `f32x8`
    The `Sum` trait was already implemented via a macro for all types but `f32x8`. I
    do not know if `f32x8` was left out intentionally or by mistake, since i cannot
    think of any optimization that can be made to this implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -464,7 +464,7 @@ macro_rules! impl_simple_sum {
 }
 
 impl_simple_sum! {
-  f32x16, f32x4, f64x8, f64x4, f64x2, i8x32, i8x16, i16x8, i16x16, i16x32, i32x8, i32x4, i32x16, i64x4, i64x2, i64x8, u8x32, u8x16, u16x8, u16x16, u16x32, u32x8, u32x4, u32x16, u64x2, u64x4, u64x8
+  f32x16, f32x8, f32x4, f64x8, f64x4, f64x2, i8x32, i8x16, i16x8, i16x16, i16x32, i32x8, i32x4, i32x16, i64x4, i64x2, i64x8, u8x32, u8x16, u16x8, u16x16, u16x32, u32x8, u32x4, u32x16, u64x2, u64x4, u64x8
 }
 
 macro_rules! impl_floating_product {
