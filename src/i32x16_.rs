63fbeed9fd3be531e3ca19644b253d8ca64cc0b3
Noam2Stein <noamsteindev@proton.me>
2026-06-29 08:04:55 +0300

    Fix existing bug: incorrect shift `rhs` mask

--- src/i32x16_.rs
+++ src/i32x16_.rs
@@ -217,7 +217,7 @@ impl Shr for i32x16 {
         use core::arch::x86_64::_mm512_srav_epi32;
 
         // Mask `rhs` to 31 to match `wrapping_shr`.
-        let rhs = bitand_m512i(rhs.avx512, set_splat_i16_m512i(31));
+        let rhs = bitand_m512i(rhs.avx512, set_splat_i32_m512i(31));
         // TODO(safe_arch): Add `_mm512_srav_epi32`.
         Self { avx512: m512i(unsafe { _mm512_srav_epi32(self.avx512.0, rhs.0) }) }
       } else {
