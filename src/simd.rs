6cebc5967d16d5d3264a697b0e23a41721251d51
Noam2Stein <noamsteindev@proton.me>
2026-06-30 14:21:24 +0300

    Move `splat` and `From<T>`

--- src/simd.rs
+++ src/simd.rs
@@ -4,11 +4,25 @@ macro_rules! impl_simd {
     N = $N:literal,
     Simd = $Simd:ident,
   ) => {
+    impl From<$T> for $Simd {
+      /// Splats the single value given across all lanes.
+      #[inline]
+      fn from(elem: $T) -> Self {
+        Self::splat(elem)
+      }
+    }
+
     impl $Simd {
       #[inline]
       #[must_use]
       pub const fn new(array: [$T; $N]) -> Self {
         unsafe { core::mem::transmute::<[$T; $N], $Simd>(array) }
+      }
+
+      #[inline]
+      #[must_use]
+      pub const fn splat(elem: $T) -> Self {
+        unsafe { core::mem::transmute::<[$T; $N], $Simd>([elem; $N]) }
       }
     }
   };
