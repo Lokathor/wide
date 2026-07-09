1785161f209e8e6bdae064243da0399e7487b314
Noam2Stein <noamsteindev@proton.me>
2026-07-09 12:22:52 +0300

    Add required test utility

--- tests/utils.rs
+++ tests/utils.rs
@@ -1,5 +1,8 @@
-pub use self::{for_simd_types::*, random_iter::*, simd_chunks::*};
+pub use self::{
+  assert_panic::*, for_simd_types::*, random_iter::*, simd_chunks::*,
+};
 
+mod assert_panic;
 mod for_simd_types;
 mod random_iter;
 mod simd_chunks;
